798134f4ef57ef0844244a0dc37604325436ea68
Mohammad Rahmani <rto1680@gmail.com>
2023-02-05 14:20:12 +0330

    Add main page design

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,211 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:shimmer/shimmer.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 500,
+            padding: EdgeInsets.only(top: 100, right: 40, left: 40, bottom: 50),
+            decoration: BoxDecoration(
+              color: Colors.red.shade100
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            width: double.infinity,
+            child: Lottie.asset("assets/88146-event-venue.json"),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Text("Schedule an event?", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
+                Shimmer.fromColors(
+                  baseColor: Colors.black,
+                  highlightColor: Colors.red,
+                  child: Text(
+                    'Schedule an event?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight:
+                      FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  children: [
+                    Shimmer.fromColors(
+                      child: Container(
+                        width: 180,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3)
+                        ),
+                        padding: EdgeInsets.all(8),
+                      ),
+                      baseColor: Colors.grey.shade200,
+                      highlightColor: Colors.grey.shade300,
+                    ),
+                    SizedBox(width: 10,),
+                    Shimmer.fromColors(
+                      child: Container(
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3)
+                        ),
+                        padding: EdgeInsets.all(8),
+                      ),
+                      baseColor: Colors.grey.shade200,
+                      highlightColor: Colors.grey.shade300,
+                    )
+                  ]
+                ),
+                SizedBox(height: 16,),
+                Row(
+                  children: [
+                    Shimmer.fromColors(
+                      child: Container(
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3)
+                        ),
+                        padding: EdgeInsets.all(8),
+                      ),
+                      baseColor: Colors.grey.shade200,
+                      highlightColor: Colors.grey.shade300,
+                    ),
+                    SizedBox(width: 10,),
+                    Shimmer.fromColors(
+                      child: Container(
+                        width: 250,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3)
+                        ),
+                        padding: EdgeInsets.all(8),
+                      ),
+                      baseColor: Colors.grey.shade200,
+                      highlightColor: Colors.grey.shade300,
+                    )
+                  ]
+                ),
+                SizedBox(height: 16,),
+                Row(
+                  children: [
+                    Shimmer.fromColors(
+                      child: Container(
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3)
+                        ),
+                        padding: EdgeInsets.all(8),
+                      ),
+                      baseColor: Colors.grey.shade200,
+                      highlightColor: Colors.grey.shade300,
+                    ),
+                    SizedBox(width: 10,),
+                    Shimmer.fromColors(
+                      child: Container(
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3)
+                        ),
+                        padding: EdgeInsets.all(8),
+                      ),
+                      baseColor: Colors.grey.shade200,
+                      highlightColor: Colors.grey.shade300,
+                    ),
+                    SizedBox(width: 10,),
+                    Shimmer.fromColors(
+                      child: Container(
+                        width: 120,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3)
+                        ),
+                        padding: EdgeInsets.all(8),
+                      ),
+                      baseColor: Colors.grey.shade200,
+                      highlightColor: Colors.grey.shade300,
+                    )
+                  ]
+                ),
+
+                SizedBox(height: 30,),
+                Column(
+                  children: [
+                    MaterialButton(
+                      onPressed: () => {},
+                      height: 50,
+                      elevation: 0,
+                      highlightElevation: 0,
+                      minWidth: double.infinity,
+                      color: Colors.grey.shade200,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(8)
+                      ),
+                      child: Text("Today", style: TextStyle(color: Colors.red, fontSize: 18, fontWeight: FontWeight.w600),),
+                    ),
+                    SizedBox(height: 12,),
+                    MaterialButton(
+                      onPressed: () => {},
+                      height: 50,
+                      elevation: 0,
+                      highlightElevation: 0,
+                      minWidth: double.infinity,
+                      color: Colors.grey.shade200,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(8)
+                      ),
+                      child: Text("Tomorrow", style: TextStyle(color: Colors.red, fontSize: 18, fontWeight: FontWeight.w600),),
+                    ),
+                    SizedBox(height: 12,),
+                    MaterialButton(
+                      onPressed: () => {},
+                      height: 50,
+                      elevation: 0,
+                      highlightElevation: 0,
+                      minWidth: double.infinity,
+                      color: Colors.grey.shade200,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(8)
+                      ),
+                      child: Text("Next week", style: TextStyle(color: Colors.red, fontSize: 18, fontWeight: FontWeight.w600),),
+                    )
+                  ],
+                )
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        ],
+      )
     );
   }
 }
